515620b4091cf5acc4f96f9fb1febdbdd713d642
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-09-27 11:08:42 +0800

    Another receiver

--- src/receive.py
+++ src/receive.py
@@ -1,36 +1,18 @@
-import socket
-import numpy as np
-import cv2 as cv
+import cv2
+import time
 
-
-addr = ("127.0.0.1", 11111)
-buf = 512
-width = 640
-height = 480
-code = b'start'
-num_of_chunks = width * height * 3 / buf
+LOCAL_IP = '192.168.10.2'
+LOCAL_PORT_VIDEO = '8080'
 
 if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(addr)
-    while True:
-        chunks = []
-        start = False
-        while len(chunks) < num_of_chunks:
-            chunk, _ = s.recvfrom(buf)
-            if start:
-                chunks.append(chunk)
-            elif chunk.startswith(code):
-                start = True
 
-        byte_frame = b''.join(chunks)
+    addr = 'udp://' + LOCAL_IP + ':' + str(LOCAL_PORT_VIDEO)
+    cap = cv2.VideoCapture(addr)
 
-        frame = np.frombuffer(
-            byte_frame, dtype=np.uint8).reshape(height, width, 3)
-
-        cv.imshow('recv', frame)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    s.close()
-    cv.destroyAllWindows()
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            cv2.imshow("Flame", frame)
+            k = cv2.waitKey(1)
+    cap.release()
+    cv2.destroyAllWindows()
