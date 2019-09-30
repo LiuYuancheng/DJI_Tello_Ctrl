4683537c77da7577af55e9d4139fe607dd19d393
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-09-30 09:42:52 +0800

    opencv to wxwindow.

--- src/cvTowx.py
+++ src/cvTowx.py
@@ -2,9 +2,12 @@
 
 import wx
 import cv2
+LOCAL_IP = '192.168.10.2'
+LOCAL_PORT_VIDEO = '11111'
+
 
 class ShowCapture(wx.Panel):
-    def __init__(self, parent, capture, fps=15):
+    def __init__(self, parent, capture, fps=5):
         wx.Panel.__init__(self, parent)
 
         self.capture = capture
@@ -27,14 +30,21 @@ class ShowCapture(wx.Panel):
         dc.DrawBitmap(self.bmp, 0, 0)
 
     def NextFrame(self, event):
-        ret, frame = self.capture.read()
-        if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            self.bmp.CopyFromBuffer(frame)
-            self.Refresh()
+        if self.capture.isOpened():
+            ret, frame = self.capture.read()
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                self.bmp.CopyFromBuffer(frame)
+                self.Refresh()
 
 
-capture = cv2.VideoCapture(0)
+
+addr = 'udp://' + LOCAL_IP + ':' + str(LOCAL_PORT_VIDEO)
+
+#capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(addr)
+
+
 #capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 320)
 #capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 
