e7d38dff986d4cae52088830cdbbe792621742a7
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-10-10 10:55:09 +0800

    change the PATT calculate part.

--- src/telloSensor.py
+++ src/telloSensor.py
@@ -37,8 +37,8 @@ class telloSensor(threading.Thread):
         self.terminate = False
         self.verifier_checksum = None
         self.attitude = None
-
-
+        self.iterTime = -1
+        self.blockNum = 1
 
         # Show the netowrk
         for name in psutil.net_if_addrs().keys():
@@ -64,54 +64,49 @@ class telloSensor(threading.Thread):
             self.conn, addr = self.sock.accept()
             print('Connection address:'+str(addr))
             while not self.terminate:
-                self.conn.sendall(b'-1')
-                data = self.conn.recv(1024)
-                if not data: break
-                print(data)
+                if self.iterTime > 0:
+                    self.getCheckSum(self.blockNum )
+                else:
+                    data = self.getDistance()
+                    if not data: break
+                    print(data)
         print("TCP server terminat.")
 
 #-----------------------------------------------------------------------------
-    def iterCal(self, iterNum, number_of_blocks):
-        flag = -1
-        for count in range(1, iterNum+1):
-            s_b_and_s_w = self.generate_sb_and_sw()
-            seed_value = "Sb: %s, Sw %s" % (hex(s_b_and_s_w[0]), hex(s_b_and_s_w[1]))
-            print("telloSensor  : seed val: %s" %seed_value)
-            rhos = self.generate_random_block_select_list(s_b_and_s_w[0], s_b_and_s_w[1], number_of_blocks)
-            address_list = self.generate_all_address(s_b_and_s_w[1], rhos, number_of_blocks)
-            self.verifier_checksum = sigma_star = self.calculate_sigma_star(address_list)
-            while self.data < len(address_list) - 1:
-                self.client_handle(address_list)
-            if self.sigma != sigma_star:
-                flag = 1
-                self.patt_complete = True
-                print("Node is compromised")
-                break            
-        if flag == -1:
-            self.patt_complete = True
-            print ("Node is Safe")
-
-    def client_handle(self, address_list):
-        if self.patt_complete:
+    def getDistance(self):
+        """ Get the distance reading from the sensor.
+        """
+        if self.conn:
             self.conn.sendall(b'-1')
-            self.attitude = self.conn.recv(1024)
-        else:
-            try:
-                if not self.repeat_flag:
-                    self.data += 1
-                    self.address_issued_flag = True
-                self.repeat_flag = True
-                self.conn.sendall(str(address_list[self.data]).encode('utf-8'))
+            return self.conn.recv(1024)
+
+#-----------------------------------------------------------------------------
+    def getCheckSum(self, number_of_blocks):
+        s_b_and_s_w = self.generate_sb_and_sw()
+        seed_value = "Sb: %s, Sw %s" % (hex(s_b_and_s_w[0]), hex(s_b_and_s_w[1]))
+        print("telloSensor  : seed val: %s" %seed_value)
+        rhos = self.generate_random_block_select_list(s_b_and_s_w[0], s_b_and_s_w[1], number_of_blocks)
+        address_list = self.generate_all_address(s_b_and_s_w[1], rhos, number_of_blocks)
+        self.verifier_checksum = self.calculate_sigma_star(address_list) 
+        print("telloSensor : verifier check sum %s" %str(self.verifier_checksum))
+        self.sensor_checksum = self.getSensorCheckSum(address_list)
+        print("telloSensor : sensor check sum %s" %str(self.verifier_checksum))
+        if self.verifier_checksum != self.sensor_checksum:
+            self.iterTime = -1
+
+#-----------------------------------------------------------------------------
+    def getSensorCheckSum(self, address_list):
+        sigma = ''
+        for address in address_list:
+            if self.conn:
+                self.conn.sendall(str(address).encode('utf-8'))
                 ch = self.conn.recv(1024).decode('utf-8')
-                if ch != '' and ("," in ch) and (len(ch.split(',')[0]) == 2) and self.address_issued_flag:
+                if ch != '' and ("," in ch) and (len(ch.split(',')[0]) == 2):
                     sigma = sigma + ch.split(',')[0].upper()
                     self.attitude = ch.split(',')[1]
-                    self.address_issued_flag = False
-                    self.repeat_flag = False
-            except:
-                self.conn.sendall(b'-1')
-                self.attitude = self.conn.recv(1024)
+        return sigma
 
+#-----------------------------------------------------------------------------
     def calculate_sigma_star(self, address_list):
         sigma_star = ""
         f = open(gv.FILE_NAME, "r")
@@ -125,16 +120,19 @@ class telloSensor(threading.Thread):
         f.close()
         return sigma_star
 
+#-----------------------------------------------------------------------------
     def generate_sb_and_sw(self):
         random.seed(datetime.now())
         return [random.randint(gv.RANDOM_RANGE_MIN, gv.RANDOM_RANGE_MAX) for i in range(2)]
 
+#-----------------------------------------------------------------------------
     def generate_random_block_select_list(self, s_b, s_w, number_of_blocks):
         """ return a list of unduplicate number in [1, number_of_blocks]
         """
         random.seed(s_b ^ s_w)
         return random.sample(range(1, number_of_blocks+1), number_of_blocks)
 
+#-----------------------------------------------------------------------------
     def generate_all_address(self, s_w, rhos, number_of_blocks):
         number_of_words = gv.FULL_MEMORY_SIZE_NODE_MCU / number_of_blocks
         all_address_list = list()
@@ -145,6 +143,7 @@ class telloSensor(threading.Thread):
                 all_address_list.append(address)
         return all_address_list
 
+#-----------------------------------------------------------------------------
     def generate_beta_list_for_block(self, s_w, rho, number_of_words):
         random.seed(rho ^ s_w)
         beta_list = [random.randint(0, gv.WORD_SIZE - 1) for _ in range(number_of_words)]
