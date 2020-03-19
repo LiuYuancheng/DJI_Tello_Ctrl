7616668f6ed3625128925effaad10597761e50ba
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2020-03-19 12:52:39 +0800

    Added the test mode and fixed the document.

--- src/camFM/camClient.py
+++ src/camFM/camClient.py
@@ -2,7 +2,9 @@
 #-----------------------------------------------------------------------------
 # Name:        cameraClient.py
 #
-# Purpose:     This module will create a camera firmware PATT checking function
+# Purpose:     This module will create client program running on raspberry PI
+#              to capture the camera image and feed the image back to connected
+#              camera server.
 #              
 # Author:       Yuancheng Liu
 #
@@ -13,58 +15,70 @@
 
 import io
 import time
-import cv2
 import socket
 import struct
 import pickle
 
+import cv2
 import udpCom
 UDP_PORT = 5005
 BUFFER_SZ = 4096
+TEST_MD = True # test mode flag
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class camClient(object):
-    def __init__(self):
+    def __init__(self, videoSrc=0):
+        """ Init the UDP server and camera capture handler. Capture the default 
+            camera idx=0 if no video src is provided.
+            Init example : cam = camClient(videoSrc='VideoFilePath')
+        """
         self.server = udpCom.udpServer(None, UDP_PORT)
-        #self.cam = cv2.VideoCapture(0)
-        self.cam = cv2.VideoCapture('my_video.h264') #play back the presave the video.
+        print("Capture video from src: %s" %str(videoSrc))
+        self.cam = cv2.VideoCapture(videoSrc)
+        ## play back the pre-saved video if under test mode.
         self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
         self.setResolution(640, 480)
         self.data = None # image data.
 
 #-----------------------------------------------------------------------------
-    def run(self):
-        print("Server thread run() start.")
-        self.server.serverStart(handler=self.msgHandler)
-        print("Server thread run() end.")
-
-#-----------------------------------------------------------------------------
-    def setResolution(self, w, h):
-        self.cam.set(3, w)
-        self.cam.set(4, h)
-
-#-----------------------------------------------------------------------------
     def msgHandler(self, msg):
-        """ The test handler method passed into the UDP server to handle the 
-            incoming messages.
+        """ The image sending process: When the server ask the client send a new 
+            image, client send the image size to the server then cut the image data
+            based on the buffer size to several chunk, then send the chunk to the 
+            server one by one.
+            server -> new image cmd -> client
+            server <- image size <- client
+            loop:   server -> image request -> client
+                    server <- image data <- client
         """
         #print("Incomming message: %s" % str(msg))
         if msg == b'new':
             # read a new camera image data.
             _, image = self.cam.read()
-            result, frame = cv2.imencode('.jpg', image, self.encode_param)
+            _, frame = cv2.imencode('.jpg', image, self.encode_param)
             self.data = pickle.dumps(frame, 0)
-            size = len(self.data)
-            msg = str(size)
+            msg = str(len(self.data))   # Image size.
         else:
             if len(self.data) > BUFFER_SZ:
                 msg = self.data[:BUFFER_SZ]
                 self.data = self.data[BUFFER_SZ:]
             else:
                 msg = self.data
-            print(len(msg))
         return msg
+
+#-----------------------------------------------------------------------------
+    def run(self):
+        """ Main incomming message handle loop. """
+        print("Camera client run() start.")
+        self.server.serverStart(handler=self.msgHandler)
+        print("Camera client run() end.")
+
+#-----------------------------------------------------------------------------
+    def setResolution(self, w, h):
+        """ Set the feed back image resulotion. """
+        self.cam.set(3, w)
+        self.cam.set(4, h)
 
 #-----------------------------------------------------------------------------
     def termiate(self):
@@ -73,7 +87,8 @@ class camClient(object):
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 def main():
-    cam = camClient()
+    videoSrc = 'my_video.h264' if TEST_MD else 0
+    cam = camClient(videoSrc=videoSrc)
     cam.run()
 
 #-----------------------------------------------------------------------------
